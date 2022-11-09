d73e98919e3ba7efc04df61894289177f1b088a5
shar <Sasha.sharai1998@mail.ru>
2022-11-09 21:03:59 +0300

    registration all models account_journal in admin.py

--- manager/account_journal/admin.py
+++ manager/account_journal/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import IncomeNote,ExpenseNote,Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_filter = ('id','title')
+    list_display_links = ('id','title')
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'money', 'organization', 'date_created')
+    list_filter = ('id', 'money', 'date_created')
+    list_display_links = ('id', 'money', 'date_created')
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'money', 'organization', 'date_created')
+    list_filter = ('id', 'money', 'date_created')
+    list_display_links = ('id', 'money', 'date_created')
+
+
+admin.site.register(IncomeNote, IncomeAdmin)
+admin.site.register(ExpenseNote, ExpenseAdmin)
+admin.site.register(Category,CategoryAdmin)
